3e6a401a13b0243474c5d01e6db06cf4cd795f64
sunil-lakshman <104969541+sunil-lakshman@users.noreply.github.com>
2023-12-08 12:01:36 +0530

    Fixed snyk issues and general code improvements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from StandardError, with: :handle_internal_server_error
+
+  def handle_internal_server_error(exception)
+    # Check if the response status is a 5XX error
+    if exception.kind_of?(ActionDispatch::ExceptionWrapper) && [500, 502, 503, 504].include?(exception.status_code)
+      # Handle 5XX errors here if needed
+      # ...
+    else
+      # Redirect to the 500.html page for other errors
+      render file: "#{Rails.root}/public/50x.html", layout: false, status: :internal_server_error
+    end
+  end
 end
